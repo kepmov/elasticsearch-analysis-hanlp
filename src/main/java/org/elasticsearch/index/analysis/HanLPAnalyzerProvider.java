c78fe7e1e53bf6bc39c6df7e5a66a8f2d20232a5
pengcong90 <pengcong@kedacom.com>
2018-05-25 17:03:30 +0800

    Update HanLPAnalyzerProvider.java

--- src/main/java/org/elasticsearch/index/analysis/HanLPAnalyzerProvider.java
+++ src/main/java/org/elasticsearch/index/analysis/HanLPAnalyzerProvider.java
@@ -19,7 +19,7 @@ public class HanLPAnalyzerProvider extends AbstractIndexAnalyzerProvider<HanLPIn
     @Inject
     public HanLPAnalyzerProvider(IndexSettings indexSettings, Environment env, @Assisted String name, @Assisted Settings settings) {
         super(indexSettings, name, settings);
-        Predefine.HANLP_PROPERTIES_PATH = sysPath.substring(0, sysPath.length()-4) + "/plugins/analysis-hanlp/hanlp.properties";
+        Predefine.HANLP_PROPERTIES_PATH = sysPath.substring(0, sysPath.length()-4) + File.separator  + "plugins" + File.separator + "analysis-hanlp" + File.separator + "hanlp.properties";
         analyzer = new HanLPIndexAnalyzer(true);
     }
 
