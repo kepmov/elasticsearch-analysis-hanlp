8d552a3b18c0f9e91c7f9dd7dc2ed13cc3cb39bf
pengcong90 <pengcong@kedacom.com>
2018-09-06 11:18:28 +0800

    Update HanLPAnalyzerProvider.java

--- src/main/java/org/elasticsearch/index/analysis/HanLPAnalyzerProvider.java
+++ src/main/java/org/elasticsearch/index/analysis/HanLPAnalyzerProvider.java
@@ -1,6 +1,5 @@
 package org.elasticsearch.index.analysis;
 
-import com.hankcs.hanlp.HanLP;
 import com.hankcs.hanlp.utility.Predefine;
 import com.hankcs.lucene4.HanLPIndexAnalyzer;
 import org.elasticsearch.common.inject.Inject;
@@ -8,18 +7,20 @@ import org.elasticsearch.common.inject.assistedinject.Assisted;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.env.Environment;
 import org.elasticsearch.index.IndexSettings;
+import java.io.File;
+import java.nio.file.Path;
 
 /**
  */
 public class HanLPAnalyzerProvider extends AbstractIndexAnalyzerProvider<HanLPIndexAnalyzer> {
 
     private final HanLPIndexAnalyzer analyzer;
-    private static String sysPath = String.valueOf(System.getProperties().get("user.dir"));
 
     @Inject
     public HanLPAnalyzerProvider(IndexSettings indexSettings, Environment env, @Assisted String name, @Assisted Settings settings) {
         super(indexSettings, name, settings);
-        Predefine.HANLP_PROPERTIES_PATH = sysPath.substring(0, sysPath.length()-4) + File.separator  + "plugins" + File.separator + "analysis-hanlp" + File.separator + "hanlp.properties";
+        Path pluginsDir = env.pluginsFile();
+        Predefine.HANLP_PROPERTIES_PATH = pluginsDir.toString() + File.separator + "analysis-hanlp" + File.separator + "hanlp.properties";
         analyzer = new HanLPIndexAnalyzer(true);
     }
 
